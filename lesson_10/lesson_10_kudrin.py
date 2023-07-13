c9ac9dbb56f49dc2507522e45f3e93d7e7b527d4
armiddqd <armpwnz@gmail.com>
2023-07-13 23:39:00 +0300

    commit

---
+++ lesson_10/lesson_10_kudrin.py
@@ -0,0 +1,4 @@
+# Task 1 - Write a program that generates 26 text files named A.txt, B.txt, and so on up to Z.txt. 
+# To each file append a random number between 1 and 100. 
+# Create a summary file (summary.txt) that contains the name of the file and the number in that file: A.txt: 67 B.txt: 12...Z.txt: 98
+
