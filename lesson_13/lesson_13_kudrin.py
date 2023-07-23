f7b15322cb37c870c4c46f1d8c8365906bc322c8
armiddqd <armpwnz@gmail.com>
2023-07-23 18:02:47 +0300

    commit

---
+++ lesson_13/lesson_13_kudrin.py
@@ -0,0 +1,4 @@
+# Task - 1 Create add method to add two countries together. 
+# This method should create another country object with 
+# the name of the two countries combined and 
+# the population of the two countries added together
